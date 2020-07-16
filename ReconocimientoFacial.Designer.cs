5eab9e769c4dd0f34fcd07e5808b11950f0b4887
Minda Yancha Wilmar Omar <wminda2042@uta.edu.ec>
2020-07-16 15:36:37 -0500

    Reconocimiento

--- ReconocimientoFacial.Designer.cs
+++ ReconocimientoFacial.Designer.cs
@@ -28,64 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox_cedula = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox_telefono = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_apellido = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_nombre = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(22, 13);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(883, 629);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Camara";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 25);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(853, 591);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox_cedula);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.textBox_id);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.textBox_telefono);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.textBox_email);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.textBox_apellido);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.textBox_nombre);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(1052, 78);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(377, 302);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Datos";
+            // 
+            // textBox_cedula
+            // 
+            this.textBox_cedula.Location = new System.Drawing.Point(132, 112);
+            this.textBox_cedula.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_cedula.Name = "textBox_cedula";
+            this.textBox_cedula.ReadOnly = true;
+            this.textBox_cedula.Size = new System.Drawing.Size(209, 27);
+            this.textBox_cedula.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 116);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 21);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Cécula";
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(132, 76);
+            this.textBox_id.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.ReadOnly = true;
+            this.textBox_id.Size = new System.Drawing.Size(209, 27);
+            this.textBox_id.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(32, 80);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(28, 21);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "ID";
+            // 
+            // textBox_telefono
+            // 
+            this.textBox_telefono.Location = new System.Drawing.Point(132, 244);
+            this.textBox_telefono.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_telefono.Name = "textBox_telefono";
+            this.textBox_telefono.ReadOnly = true;
+            this.textBox_telefono.Size = new System.Drawing.Size(209, 27);
+            this.textBox_telefono.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 247);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 21);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Teléfono";
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(132, 212);
+            this.textBox_email.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.ReadOnly = true;
+            this.textBox_email.Size = new System.Drawing.Size(209, 27);
+            this.textBox_email.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 215);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 21);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Email";
+            // 
+            // textBox_apellido
+            // 
+            this.textBox_apellido.Location = new System.Drawing.Point(132, 180);
+            this.textBox_apellido.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_apellido.Name = "textBox_apellido";
+            this.textBox_apellido.ReadOnly = true;
+            this.textBox_apellido.Size = new System.Drawing.Size(209, 27);
+            this.textBox_apellido.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 183);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 21);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Apellido";
+            // 
+            // textBox_nombre
+            // 
+            this.textBox_nombre.Location = new System.Drawing.Point(132, 148);
+            this.textBox_nombre.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_nombre.Name = "textBox_nombre";
+            this.textBox_nombre.ReadOnly = true;
+            this.textBox_nombre.Size = new System.Drawing.Size(209, 27);
+            this.textBox_nombre.TabIndex = 13;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(32, 151);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 21);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Nombre";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(267, 219);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(1246, 424);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 101);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 42);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Capturar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(60, 41);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 1;
-            // 
-            // monthCalendar1
-            // 
-            this.monthCalendar1.Location = new System.Drawing.Point(386, 113);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 2;
-            // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(126, 223);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(121, 97);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ReconocimientoFacial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.monthCalendar1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(1587, 1055);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Name = "ReconocimientoFacial";
-            this.Text = "Form2";
+            this.Text = "Reconocimiento Pacientes con Covid-19";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ReconocimientoFacial_FormClosing);
+            this.Load += new System.EventHandler(this.ReconocimientoFacial_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox_cedula;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox_telefono;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_apellido;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_nombre;
+        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.MonthCalendar monthCalendar1;
-        private System.Windows.Forms.ListView listView1;
     }
 }
--- ReconocimientoFacial.cs
+++ ReconocimientoFacial.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Luxand;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,331 @@ namespace Covid_19ReconocimientoFacial
         public ReconocimientoFacial()
         {
             InitializeComponent();
+        }
+
+
+        #region Variables
+
+        // program states: whether we recognize faces, or user has clicked a face
+        enum ProgramState { psRemember, psRecognize }
+        ProgramState programState = ProgramState.psRecognize;
+
+        String cameraName;
+        bool needClose = false;
+        string userName;
+        String TrackerMemoryFile = "tracker.dat";
+        int mouseX = 0;
+        int mouseY = 0;
+
+        // WinAPI procedure to release HBITMAP handles returned by FSDKCam.GrabFrame
+        [DllImport("gdi32.dll")]
+        static extern bool DeleteObject(IntPtr hObject);
+
+        #endregion
+
+        bool estaGuardado = false;
+        List<Usuario> listaUsuarios = new List<Usuario>();
+        Usuario usuario = new Usuario();
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Capturar();
+        }
+
+        /// <summary>
+        /// metodos
+        /// </summary>
+
+
+        private void CargarUsuarioDB()
+        {
+            listaUsuarios.Clear();
+            listaUsuarios = UsuarioDatos.CargarListaUsuario(listaUsuarios);
+        }
+
+        private Usuario BuscarUsuario(string name)
+        {
+            //Debug.WriteLine("name: " + name);
+            foreach (Usuario item in listaUsuarios)
+            {
+                if (item.Cedula.Equals(name))
+                {
+                    //Debug.WriteLine("encontro: ced: " + item.Cedula);
+                    return item;
+                }
+            }
+            //Debug.WriteLine("no encontro");
+            return null;
+        }
+
+
+        private void CarcarCampos(Usuario usuario)
+        {
+            textBox_id.Text = usuario.Id.ToString();
+            textBox_cedula.Text = usuario.Cedula;
+            textBox_nombre.Text = usuario.Nombre;
+            textBox_apellido.Text = usuario.Apellido;
+            textBox_telefono.Text = usuario.Telefono;
+            textBox_email.Text = usuario.Email;
+        }
+
+        private void LimpiarCampos()
+        {
+            textBox_id.Text = null;
+            textBox_cedula.Text = null;
+            textBox_nombre.Text = null;
+            textBox_apellido.Text = null;
+            textBox_telefono.Text = null;
+            textBox_email.Text = null;
+        }
+
+        private void Iniciar()
+        {
+
+            if (FSDK.FSDKE_OK != FSDK.ActivateLibrary("gyYgVWQTSzjiuGB/hH8dKgg0QrrIuhoHdfUCzD9rY+vru3WRZsaezTX6YWj9osdI/cmxY1NSdLkyWuugMPCxUG7/xNLegHLeaUpzVyKpDkaWL8tJIUsIL7xv9bhmgifPbAyTDuxF3VGxXmHkv/L/MStf9kdXV/A1vVvT93QC4vQ="))
+            {
+                MessageBox.Show("Please run the License Key Wizard (Start - Luxand - FaceSDK - License Key Wizard)", "Error activating FaceSDK", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            FSDK.InitializeLibrary();
+            FSDKCam.InitializeCapturing();
+
+            #region Camaras
+            //VERIFICAR CAMARAS, OBTENER LOS NOMBRE Y LA CANTIDAD DE CAMARAS.
+            string[] cameraList;
+            int count;
+            FSDKCam.GetCameraList(out cameraList, out count);
+
+            if (0 == count)
+            {
+                MessageBox.Show("Please attach a camera", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            cameraName = cameraList[0];
+
+            #endregion
+
+            FSDKCam.VideoFormatInfo[] formatList;
+            FSDKCam.GetVideoFormatList(ref cameraName, out formatList, out count);
+
+            int VideoFormat = 0; // choose a video format
+            pictureBox1.Width = formatList[VideoFormat].Width;
+            pictureBox1.Height = formatList[VideoFormat].Height;
+
+            //Debug.WriteLine("Width: " + pictureBox1.Width);
+            //Debug.WriteLine("Height: " + pictureBox1.Height);
+
+            //this.Width = formatList[VideoFormat].Width + 75;
+            //this.Height = formatList[VideoFormat].Height + 150;
+            //Debug.WriteLine("Width: " + pictureBox1.Width);
+            //Debug.WriteLine("Height: " + pictureBox1.Height);
+
+
+        }
+
+
+        private void Capturar()
+        {
+            this.button1.Enabled = false;
+            int cameraHandle = 0;
+
+            int r = FSDKCam.OpenVideoCamera(ref cameraName, ref cameraHandle);
+            Debug.WriteLine("cameraName: " + cameraName);
+            Debug.WriteLine("cameraHandle: " + cameraHandle);
+            Debug.WriteLine("r: " + r);
+
+
+            if (r != FSDK.FSDKE_OK)
+            {
+                MessageBox.Show("Error opening the first camera", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            int tracker = 0;
+            // creating a Tracker
+            if (FSDK.FSDKE_OK != FSDK.LoadTrackerMemoryFromFile(ref tracker, TrackerMemoryFile)) // try to load saved tracker state
+                FSDK.CreateTracker(ref tracker); // if could not be loaded, create a new tracker
+
+            int err = 0; // set realtime face detection parameters
+            FSDK.SetTrackerMultipleParameters(tracker, "HandleArbitraryRotations=false; DetermineFaceRotationAngle=false; InternalResizeWidth=100; FaceDetectionThreshold=5;", ref err);
+
+            while (!needClose)
+            {
+                Int32 imageHandle = 0;
+                if (FSDK.FSDKE_OK != FSDKCam.GrabFrame(cameraHandle, ref imageHandle)) // grab the current frame from the camera
+                {
+                    Application.DoEvents();
+                    continue;
+                }
+
+                FSDK.CImage image = new FSDK.CImage(imageHandle);
+
+
+                long[] IDs;
+                long faceCount = 0;
+                FSDK.FeedFrame(tracker, 0, image.ImageHandle, ref faceCount, out IDs, sizeof(long) * 256); // maximum of 256 faces detected
+                Array.Resize(ref IDs, (int)faceCount);
+
+                // make UI controls accessible (to find if the user clicked on a face)
+                Application.DoEvents();
+
+                Image frameImage = image.ToCLRImage();
+                Graphics gr = Graphics.FromImage(frameImage);
+                //pictureBox2.Image = frameImage;
+
+                if (IDs.Length == 0)
+                {
+                    LimpiarCampos();
+                }
+
+                for (int i = 0; i < IDs.Length; ++i)
+                {
+                    //Debug.WriteLine("Cant: " + IDs.Length);
+                    FSDK.TFacePosition facePosition = new FSDK.TFacePosition();
+                    FSDK.GetTrackerFacePosition(tracker, 0, IDs[i], ref facePosition);
+
+                    int left = facePosition.xc - (int)(facePosition.w * 0.6);
+                    int top = facePosition.yc - (int)(facePosition.w * 0.5);
+                    int w = (int)(facePosition.w * 1.2);
+
+                    String name;
+                    int res = FSDK.GetAllNames(tracker, IDs[i], out name, 65536); // maximum of 65536 characters
+
+                    //DETECCION
+
+                    if (FSDK.FSDKE_OK == res)
+                    { // draw name
+
+                        if (name.Length <= 0)
+                        {
+                            name = "Desconocido";
+                            usuario = new Usuario();
+                            LimpiarCampos();
+                        }
+                        else
+                        {
+                            usuario = BuscarUsuario(name);
+                            CarcarCampos(usuario);
+                            name = usuario.Nombre + " " + usuario.Apellido;
+                        }
+
+                        StringFormat format = new StringFormat();
+                        format.Alignment = StringAlignment.Center;
+
+                        gr.DrawString(name, new Font("Century Gothic", 16, FontStyle.Bold),
+                            new SolidBrush(Color.LightGreen),
+                            facePosition.xc, top + w + 5, format);
+                    }
+
+
+                    //REGISTRO
+                    Pen pen = Pens.DarkGreen;
+                    //if (mouseX >= left && mouseX <= left + w && mouseY >= top && mouseY <= top + w)
+                    //{
+                    //    pen = Pens.DarkBlue;
+                    //    if (ProgramState.psRemember == programState)
+                    //    {
+                    //        if (FSDK.FSDKE_OK == FSDK.LockID(tracker, IDs[i]))
+                    //        {
+                    //            // get the user name
+                    //            ////CamposBloqueados(false);
+                    //            //Debug.WriteLine("desbloqueo campos");
+                    //            //Debug.WriteLine("estaGuar: " + estaGuardado);
+
+                    //            inputDatos inputDatos = new inputDatos();
+                    //            bool nuevoUsuario = false;
+
+                    //            if (usuario.Id <= 0)
+                    //            {
+                    //                Debug.WriteLine("Nuevo usuario");
+                    //                nuevoUsuario = true;
+                    //            }
+                    //            else
+                    //            {
+                    //                Debug.WriteLine("Nombre: " + usuario.Nombre + "\tCedula: " + usuario.Cedula);
+                    //                inputDatos.setDatos(usuario);
+                    //                nuevoUsuario = false;
+                    //            }
+
+
+
+
+                    //            if (DialogResult.OK == inputDatos.ShowDialog())
+                    //            //if (estaGuardado)
+                    //            {
+                    //                Debug.WriteLine("entró if");
+                    //                //userName = textBox_cedula.Text;
+                    //                usuario = inputDatos.usuario;
+
+                    //                //userName = inputName.userName;
+                    //                userName = usuario.Cedula;
+
+                    //                if (userName == null || userName.Length <= 0)
+                    //                {
+                    //                    String s = "";
+                    //                    FSDK.SetName(tracker, IDs[i], "");
+                    //                    FSDK.PurgeID(tracker, IDs[i]);
+                    //                }
+                    //                else
+                    //                {
+                    //                    FSDK.SetName(tracker, IDs[i], userName);
+                    //                }
+                    //                FSDK.UnlockID(tracker, IDs[i]);
+
+                    //                usuario.Imagen = frameImage;
+
+                    //                if (nuevoUsuario)
+                    //                {
+                    //                    usuario = Usuario_Datos.insertUsuario(usuario);
+                    //                    MessageBox.Show("Registro guardado correctamente");
+                    //                }
+                    //                else
+                    //                {
+                    //                    Debug.WriteLine("id: " + usuario.Id);
+                    //                    Debug.WriteLine("ced: " + usuario.Cedula);
+                    //                    usuario = Usuario_Datos.updateUsuario(usuario);
+                    //                    MessageBox.Show("Registro actualizado correctamente");
+                    //                }
+
+                    //                CargarUsuarioDB();
+
+                    //                //CamposBloqueados(true);
+                    //            }
+                    //            estaGuardado = false;
+                    //        }
+                    //    }
+                    //}
+
+                    gr.DrawRectangle(pen, left, top, w, w);
+                }
+                programState = ProgramState.psRecognize;
+
+                // display current frame
+                pictureBox1.Image = frameImage;
+                GC.Collect(); // collect the garbage after the deletion
+            }
+
+            FSDK.SaveTrackerMemoryToFile(tracker, TrackerMemoryFile);
+            FSDK.FreeTracker(tracker);
+
+
+            FSDKCam.CloseVideoCamera(cameraHandle);
+            FSDKCam.FinalizeCapturing();
+
+        }
+
+        private void ReconocimientoFacial_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            needClose = true;
+        }
+
+        private void ReconocimientoFacial_Load(object sender, EventArgs e)
+        {
+            CargarUsuarioDB();
+            Iniciar();
         }
     }
 }
